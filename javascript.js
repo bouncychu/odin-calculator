81d06f737b98e184d4b4c4b3b05488aad8a63cd5
bouncychu <v.y.h.tong@gmail.com>
2024-02-14 15:33:18 +0800

    Update functions for plus, minus times and divide

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
 //javascript for odin project calculator
+
+let add = (numberA, numberB) => numberA + numberB;
+
+let subtract = (numberA, numberB) => numberA - numberB;
+
+let multiply = (numberA, numberB) => numberA * numberB;
+
+let divide = (numberA, numberB) => numberA / numberB;
+
+
